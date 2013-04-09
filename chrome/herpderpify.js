568d0a2d2b20ca20660b5bd28e0a4714ef0d5d6f
zemm <zemm@iki.fi>
2013-04-09 18:22:49 +0300

    refactoring, take image bounds from element if visible, closes #3

--- chrome/herpderpify.js
+++ chrome/herpderpify.js
@@ -6,46 +6,44 @@ as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
 */
 
 (function(){
-	var imgMinH = 50;
-	var imgMaxH = 200;
-	var imgMinW = 100;
-	var imgMaxW = 400;
-	var classNames = [
-		//'author',
-		'comment-body',
-		'xcTextValue'
-	];
+	var minH        = 50;
+	var minVarH     = 100;
+	var defaultVarH = 150;
+	var minW        = 100;
+	var minVarW     = 100;
+	var defaultVarW = 300; // when not found (hidden elements)
+
+	var selector = '.comment-body, .xcTextValue';
 
 	var checkComments = function() {
 		var els = findUnderpedElements();
-		for (var i in els) {
-			if ( ! els[i].derpedHTML) {
-				makeElementDerped(els[i]);
-			}
-		}
+		for (
+			var i = 0, c = els.length;
+			i !== c;
+			makeElementDerped(els[i++])
+		);
 		setTimeout(checkComments, 300);
 	};
 
 	var findUnderpedElements = function() {
-		var els = [];
-		for (var i in classNames) {
-			var domNodeList = document.getElementsByClassName(classNames[i]);
-			for (
-				var j = 0,
-					c = domNodeList.length;
-				j != c;
-				els.push(domNodeList[j++])
-			);
+		var res = [];
+		var els = document.querySelectorAll(selector);
+		var el;
+		for (var i = 0, c = els.length; i != c; ++i) {
+			el = els[i];
+			if ( ! el.derpedHTML) {
+				res.push(el);
+			}
 		}
-		return els;
+		return res;
 	};
 
 	var makeElementDerped = function(el) {
-		var generatorI = Math.floor(Math.random() * generators.length);
-		el.originalHTML = el.innerHTML;
-		el.derpedHTML = generators[generatorI](el);
-		el.innerHTML = el.derpedHTML;
-		el.isDerped = true;
+		var generatorIndex = Math.floor(Math.random() * generators.length);
+		el.originalHTML    = el.innerHTML;
+		el.derpedHTML      = generators[generatorIndex](el);
+		el.innerHTML       = el.derpedHTML;
+		el.isDerped        = true;
 		el.addEventListener('click', onElementClick, true);
 	};
 
@@ -57,10 +55,10 @@ as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
 		if (el.derpedHTML) {
 			if (el.isDerped) {
 				el.innerHTML = el.originalHTML;
-				el.isDerped = false;
+				el.isDerped  = false;
 			} else {
 				el.innerHTML = el.derpedHTML;
-				el.isDerped = true;
+				el.isDerped  = true;
 			}
 		}
 	};
@@ -70,7 +68,7 @@ as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
 		function(el) {
 			var herpderps = [];
 			var original = el.innerHTML;
-			for (var i=0,c=countWords(original); i<c; ++i) {
+			for (var i = 0, c = countWords(original); i != c; ++i) {
 				herpderps.push((Math.random() > 0.5) ? 'herp' : 'derp');
 			}
 			return herpderps.join(' ');
@@ -78,9 +76,8 @@ as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
 
 		// kittens
 		function(el) {
-			var w = imgMinW + Math.floor(Math.random() * (imgMaxW - imgMinW));
-			var h = imgMinH + Math.floor(Math.random() * (imgMaxH - imgMinH));
-			return '<img src="http://placekitten.com/' + w + '/' + h + '" />';
+			var size = generateImageSize(el);
+			return '<img src="http://placekitten.com/' + size[0] + '/' + size[1] + '" />';
 		}
 	];
 
@@ -88,6 +85,21 @@ as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
 		return string.replace(/[\s\n]+/g, ' ').split(' ').length;
 	};
 
-	checkComments();
+	var generateImageSize = function(el) {
+		var varW, varH;
+		if (el.offsetWidth > 0) {
+			varW = Math.max(minVarW, el.offsetWidth - minW);
+			varH = Math.max(minVarH, el.offsetHeight - minH);
+		} else {
+			varW = defaultVarW;
+			varH = defaultVarH;
+		}
+		return [
+			minW + Math.floor(Math.random() * varW),
+			minH + Math.floor(Math.random() * varH)
+		];
+	};
+
+	setTimeout(checkComments, 100);
 
 })();
